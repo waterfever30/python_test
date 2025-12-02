61e695356dd3a7c4f6a5e6de6ac8310551f5d6f6
waterfever30 <waterfever30@gmail.com>
2025-12-01 21:12:28 -0500

    added to github

---
+++ calc.test.py
@@ -0,0 +1,7 @@
+def test_sum():
+    sum = 2 + 2
+    assert sum == 4
+
+    def test2_sum():
+        sum = 2 + 2
+        assert sum == 5
---
+++ interview_questions.py
@@ -0,0 +1,12 @@
+# Test. Verification of compute_factorial function
+def test_compute_factorial():
+    # Define the input number
+    input_number = 5
+
+    # Perform the factorial computation
+    result = compute_factorial(input_number)
+
+    # Check if the result is equal to the expected factorial value
+    assert result == 120
+
+    print("Test Passed! The factorial of " + str(input_number) + " is " + str(result))
